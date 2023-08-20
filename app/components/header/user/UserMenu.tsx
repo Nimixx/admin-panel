de200b58633f8ac14a46379e8cb46fd8ec657180
nimixx <nimixx.dev@proton.me>
2023-08-20 21:12:05 +0200

    Rebuild with radix ui

--- app/components/header/user/UserMenu.tsx
+++ app/components/header/user/UserMenu.tsx
@@ -1,29 +1,39 @@
-'use client'
-
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styles from './userMenu.module.scss'
-import { ChevronDown } from 'lucide-react'
-import Navigation from './navigation/Navigation'
-import { useEscapeKey } from '../../hooks/useEscapeKey'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { useThemeContext } from '@/app/context/ThemeContext'
 
 export default function UserMenu() {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggleMenu = () => {
-    setIsOpen((prevState) => !prevState)
-  }
-
-  useEscapeKey({ enabled: isOpen, onClose: toggleMenu });
+  const { theme } = useThemeContext()
 
   return (
-    <section className={styles.container}>
-      <button onClick={toggleMenu} className={styles.profile__btn}>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild className={styles.profile__btn} aria-label="Customise options">
         <img className={styles.avatar} src="avatar.jpg" alt="user profile avatar" />
-        <ChevronDown
-          className={`${styles.arrow} ${isOpen ? styles.arrow__open : styles.arrow__close}`}
-        />
-      </button>
-      {isOpen ? <Navigation /> : null}
-    </section>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          data-theme={theme}
+          className={styles.content}
+          sideOffset={5}
+          align="end"
+        >
+          <DropdownMenu.Label className={styles.userName}>Josh</DropdownMenu.Label>
+          <DropdownMenu.Separator className={styles.separator} />
+          <DropdownMenu.Group className={styles.group}>
+            <DropdownMenu.Label className={styles.label}>Profile</DropdownMenu.Label>
+            <DropdownMenu.Item className={styles.item}>Information</DropdownMenu.Item>
+            <DropdownMenu.Item className={styles.item}>Settings</DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Separator className={styles.separator} />
+          <DropdownMenu.Group className={styles.group}>
+            <DropdownMenu.Label className={styles.label}>Help</DropdownMenu.Label>
+            <DropdownMenu.Item className={styles.item}>FAQ</DropdownMenu.Item>
+            <DropdownMenu.Item className={styles.item}>Contacts</DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Arrow className={styles.arrow} />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   )
 }
