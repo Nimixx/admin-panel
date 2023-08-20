416a9aa71381e2bb08fc84d3758854d0ba506a91
nimixx <nimixx.dev@proton.me>
2023-08-20 15:10:47 +0200

    Add nav links with map

--- app/components/header/user/UserMenu.tsx
+++ app/components/header/user/UserMenu.tsx
@@ -4,11 +4,9 @@ import styles from './userMenu.module.scss'
 import { ChevronDown } from 'lucide-react'
 
 import { useState } from 'react'
-import { useThemeContext } from '@/app/context/ThemeContext'
 import Navigation from './navigation/Navigation'
 
 export default function UserMenu() {
-  const { theme } = useThemeContext()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
@@ -16,7 +14,7 @@ export default function UserMenu() {
   }
 
   return (
-    <section data-theme={theme} className={styles.container}>
+    <section className={styles.container}>
       <button onClick={toggleMenu} className={styles.profile__btn}>
         <img className={styles.avatar} src="avatar.jpg" alt="user profile avatar" />
         <ChevronDown
--- app/components/ui/NavSpacer.tsx
+++ app/components/ui/NavSpacer.tsx
@@ -1,8 +1,5 @@
 import styles from './style/navSpacer.module.scss'
-import { useThemeContext } from '@/app/context/ThemeContext'
 
 export default function NavSpacer() {
-  const { theme } = useThemeContext()
-
-  return <div data-theme={theme} className={styles.spacer} />
+  return <div className={styles.spacer} />
 }
